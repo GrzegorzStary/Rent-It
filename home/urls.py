51a1649a24702a7758401caf90c38a1c0de80c9e
Grzegorz Stary <stary.grzegorz@gmail.com>
2025-06-25 16:42:14 +0100

    ADDED: Styling of homepage, nav bar, footer, hero image

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Index
+from .views import Index, search_view
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
+    path('search/', search_view, name='search'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Items
 
 # Create your views here.
 
 class Index(TemplateView):
     template_name = 'home/index.html'
 
+def search_view(request):
+    query = request.GET.get('q', '')
+    results = []
     
+    if query:
+        results = Items.objects.filter(name__icontains=query)
+        
+    return render(request, 'search_results.html', {
+        query: query,
+        'results': results
+    })
