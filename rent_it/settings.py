7766ba06ccc1a90afc488895b01bfa6dd2ae5f21
Grzegorz Stary <stary.grzegorz@gmail.com>
2025-06-26 17:03:46 +0100

    STYLING: Navbar My account dropdown

--- rent_it/settings.py
+++ rent_it/settings.py
@@ -85,7 +85,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'templates',
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
             ],
         'APP_DIRS': True,
         'OPTIONS': {
