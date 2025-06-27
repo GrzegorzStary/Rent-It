0bd58c0852de2dcd4a54850271f79ba8d6a3a846
Grzegorz Stary <stary.grzegorz@gmail.com>
2025-06-27 03:50:23 +0100

    COMPLETED: Nav-bar on large and small screen - responsive neatly

--- rent_it/settings.py
+++ rent_it/settings.py
@@ -177,3 +177,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+# All uploaded files are stored in the 'media' directory
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
