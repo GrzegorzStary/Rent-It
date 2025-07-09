617c1d8259df58d5ade39a81fc02f5d50f70570b
Grzegorz Stary <stary.grzegorz@gmail.com>
2025-07-09 08:32:34 +0300

    ADDED: terms_of_service.html

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Index, search_view, privacy_policy
+from .views import Index, search_view, privacy_policy, terms_of_service
 from home.views import home
 
 urlpatterns = [
     path('home/', home, name='home'),
     path('search/', search_view, name='search'),
     path('privacy-policy/', privacy_policy, name='privacy_policy'),
+    path('terms-of-service/', terms_of_service, name='terms_of_service'),
 ]
--- home/views.py
+++ home/views.py
@@ -28,3 +28,6 @@ def home(request):
 
 def privacy_policy(request):
     return render(request, 'home/privacy_policy.html')
+
+def terms_of_service(request):
+    return render(request, 'home/terms_of_service.html')
